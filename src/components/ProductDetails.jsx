2842228f13ee5c5d4dc89e74cbdf25c632b7201f
Eraulia Nadinda Rachmat <dinda.rachmat@gmail.com>
2019-03-06 13:28:08 +0700

    [Dinda] delete double components and update dependencies

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -11,15 +11,15 @@ export default class ProductDetails extends Component {
     constructor(props) {
         super(props);
   
-    componentDidMount() {
-        let productId = localStorage.getItem('JWT_TOKEN')
-        axios.get(`https://mgvplus.herokuapp.com/products/${productId}`).then(response => {
-            const productName = response.data.productName;
-            const productPrice = response.data.productPrice;
-            this.setState({productName, productPrice});
-        });
-        localStorage.getItem('JWT_TOKEN')
-      }
+    // componentDidMount() {
+    //     let productId = localStorage.getItem('JWT_TOKEN')
+    //     axios.get(`https://mgvplus.herokuapp.com/products/${productId}`).then(response => {
+    //         const productName = response.data.productName;
+    //         const productPrice = response.data.productPrice;
+    //         this.setState({productName, productPrice});
+    //     });
+    //     localStorage.getItem('JWT_TOKEN')
+    //   }
     }
 
     render() {
